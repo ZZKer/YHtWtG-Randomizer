c53fe2a20c7b8f37c8fcdd538ea0761844d71ec7
ZZKer <Kyle.Mumenthaler@gmail.com>
2020-02-05 11:53:00 -0500

    Update and rename RandomizeYHTWTGv1_0.py to RandomizeYHTWTGv1_1.py
    Version 1.1 fixes most softlocks with an improved map as well as getting rid of certain checkpoints based on orb placement. Only known softlock remaining is getting red orb on Hollow King, immediately going down to Green Man and getting blue orb or spider gloves. This softlock can be avoided by always going right at the beginning instead of left.

--- RandomizeYHTWTGv1_1.py
+++ RandomizeYHTWTGv1_1.py
@@ -17,13 +17,17 @@ ending = ['" toptitle="Cerulean Aura" title="- Activates Blue Ghost Blocks -">\n
 writefilename = 'Rooms_random' + seed + '.xml'
 readfile = open('Rooms_Normal.xml')
 writefile = open(writefilename, 'w')
+times_popped = 0
 for line in readfile:
-	if('toptitle' in line):
-		activepair = orbs_r.pop(0)
-		newline = '<room x="'+str(activepair[0])+'" y="'+str(activepair[1])+ending.pop(0)
-		writefile.write(newline)
-	else:
-		writefile.write(line)
+      if('toptitle' in line):
+            activepair = orbs_r.pop(0)
+            newline = '<room x="'+str(activepair[0])+'" y="'+str(activepair[1])+ending.pop(0)
+            writefile.write(newline)
+            if(activepair[0]==5 and times_popped>0 and times_popped<3):
+                        writefile.write(' <entity template="bell" x="300" y ="150" />\n')
+            times_popped += 1
+      else:
+            writefile.write(line)
 readfile.close()
 writefile.close()
 
@@ -32,14 +36,14 @@ gamfilename = 'random' + seed + '.gam'
 writefile = open(gamfilename, 'w')
 gam_text = 'Title: Rand' + seed + '\n'
 gam_text += 'FullTitle: Randomizer seed ' + seed +'\n'
-gam_text += "DevTeam: ZZKylie / ZZKer" + '\n'
+gam_text += 'DevTeam: ZZKylie and the WtG Speedrun Discord\n'
 gam_text += 'DevLogo: phearts.bmp\n'
 gam_text += "Author: ZZKylie / ZZKer" + '\n'
 gam_text += "URL: ZZKer#1962" + '\n'
-gam_text += 'Description: Orb Randomizer version 1.0\n'
+gam_text += 'Description: Orb Randomizer version 1.1\n'
 gam_text += 'MajorVersion: 1\n'
-gam_text += 'MinorVersion: 0\n'
-gam_text += 'Map: yhtwtg.map\n'
+gam_text += 'MinorVersion: 1\n'
+gam_text += 'Map: randomBase.map\n'
 gam_text += 'Content: DefaultContent.ini\n'
 gam_text += 'Rooms: ' + writefilename + '\n'
 gam_text += 'StartX: -3\nStartY: 0\n'
