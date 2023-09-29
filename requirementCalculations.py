3ed8aa006fb5a5db9028b8f99c03c5c75c7ea9ab
spontanicus <david.neisens@gmail.com>
2023-09-30 01:15:16 +0200

    Some more test-code for rando logic
    Also fixed a few errors in the matrix.
    Using the standard map matrix it and the spawn location as the only initially reachable location as input it's now possible to calculate that it's indeed necessary to collect all 4 powerups to reach the end from the start of the game

--- requirementCalculations.py
+++ requirementCalculations.py
@@ -64,3 +64,54 @@ def readTable(file):
                         table[row][col] += [int(entry)]
             row += 1
     return table
+
+
+def writeState(writeFile, states, stateNameFile):
+    stateNames = []
+    writeNames = False
+    with open(stateNameFile) as readFile:
+        firstLineSplits = readFile.readline().split(";")
+        while len(firstLineSplits) > 0 and not firstLineSplits[0].strip():
+            firstLineSplits = firstLineSplits[1:]
+        if len(firstLineSplits) > 0 and not firstLineSplits[0].isnumeric():
+            stateNames = firstLineSplits
+            writeNames = True
+    with open(writeFile, 'w') as writeFile:
+        if writeNames:
+            writeFile.write(writeTableLine(stateNames))
+
+        writeFile.write(writeTableLine(states))
+
+
+def writeTableLine(entries):
+    writeLine = ""
+    if len(entries) == 0:
+        print("no entries to write")
+        return ""
+
+    writeLine += writeReqEntry(entries[0])
+    if len(entries) > 1:
+        for entry in entries[1:]:
+            writeLine += ";" + writeReqEntry(entry)
+
+    return writeLine
+
+
+def writeReqEntry(entry):
+    writeReq = ""
+    if len(entry) == 0:
+        return ""
+
+    if isinstance(entry, str):
+        return entry
+
+    writeReq += str(entry[0])
+    if len(entry) == 1:
+        return writeReq
+
+    for req in entry[1:]:
+        writeReq += "," + str(req)
+
+    return writeReq
+
+
