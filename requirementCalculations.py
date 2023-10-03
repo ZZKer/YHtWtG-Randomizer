9e987bfa7c8ee32998be2a13dc9a1e19959ecedc
spontanicus <david.neisens@gmail.com>
2023-10-03 17:12:07 +0200

    Calculate reduced connection matrix
    I added a bunch of functions to work with the connection matrix and compress it into a format the randomizer can use for generating a map. Since we're only interested in which pickup can be reached from which other pickup (to automatically check if a set of powerup locations can be solved or not). A reduced connection matrix with only the pickup locations was added.
    
    At the moment the matrix reduction code is way too slow to be used for a full room randomizer, but for everything else we can precalculate the matrices needed to generate maps.
    
    Also made some corrections in the full matrix

--- requirementCalculations.py
+++ requirementCalculations.py
@@ -1,3 +1,11 @@
+DEFAULT_SPAWN = "You Have to Start the Game Spawn"
+DEFAULT_REDORB = "Crimson Aura Pickup"
+DEFAULT_BLUEORB ="Cerulean Aura Pickup"
+DEFAULT_BOOTS = "Springheel Boots Pickup"
+DEFAULT_GLOVES = "Spider Gloves Pickup"
+DEFAULT_LOSE = "Consolation Prize Pickup-1"
+DEFAULT_WIN = "Eponymous Pickup"
+
 def reduceReqs(reqs):
     reducedReqs = []
     reqs.sort()
@@ -48,62 +56,69 @@ def readTable(file):
         return []
 
     table = [[[] for _ in range(nrRows)] for _ in range(nrCols)]
+    labels = ["" for _ in range(nrCols)]
 
     with open(file) as tableFile:
         tableFile.readline()
+
         row = 0
         for line in tableFile:
             if not line.strip():
                 continue
 
-            lineSplits = line.split(";")[1:]
+            lineSplits = line.split(";")
+            labels[row] = lineSplits[0]
+            lineSplits = lineSplits[1:]
             for col in range(len(lineSplits)):
                 entries = lineSplits[col].split(",")
                 for entry in entries:
                     if entry.strip() and int(entry) >= 0:
                         table[row][col] += [int(entry)]
             row += 1
-    return table
+    return table, labels
 
 
-def writeState(writeFile, states, stateNameFile):
-    stateNames = []
-    writeNames = False
-    with open(stateNameFile) as readFile:
-        firstLineSplits = readFile.readline().split(";")
-        while len(firstLineSplits) > 0 and not firstLineSplits[0].strip():
-            firstLineSplits = firstLineSplits[1:]
-        if len(firstLineSplits) > 0 and not firstLineSplits[0].isnumeric():
-            stateNames = firstLineSplits
-            writeNames = True
+def writeTable(writeFile, states, stateNames=None):
+    if isinstance(stateNames, str):
+        stateNames = getStateListFromFile(stateNames)
     with open(writeFile, 'w') as writeFile:
-        if writeNames:
-            writeFile.write(writeTableLine(stateNames))
+        if len(stateNames) > 0:
+            writeFile.write(";" + getTableLine(stateNames))
+            writeFile.write("\n")
 
-        writeFile.write(writeTableLine(states))
+        for i in range(len(states)):
+            if len(stateNames) > i:
+                line = [stateNames[i]] + states[i]
+            else:
+                line = [""] + states[i]
+            writeFile.write(getTableLine(line))
+            writeFile.write("\n")
 
 
-def writeTableLine(entries):
+def getTableLine(entries):
     writeLine = ""
     if len(entries) == 0:
         print("no entries to write")
         return ""
 
-    writeLine += writeReqEntry(entries[0])
+    writeLine += getTableEntry(entries[0])
     if len(entries) > 1:
         for entry in entries[1:]:
-            writeLine += ";" + writeReqEntry(entry)
+            writeLine += ";" + getTableEntry(entry)
 
     return writeLine
 
 
-def writeReqEntry(entry):
+def getTableEntry(entry):
     writeReq = ""
     if len(entry) == 0:
         return ""
 
     if isinstance(entry, str):
-        return entry
+        if entry.startswith("\"") and entry.endswith("\""):
+            return entry
+        else:
+            return "\"" + entry + "\""
 
     writeReq += str(entry[0])
     if len(entry) == 1:
@@ -114,4 +129,72 @@ def writeReqEntry(entry):
 
     return writeReq
 
+
+def getStateListFromFile(stateNameFile):
+    stateNames = []
+    with open(stateNameFile) as readFile:
+        firstLineSplits = readFile.readline().split(";")
+        while len(firstLineSplits) > 0 and not firstLineSplits[0].strip():
+            firstLineSplits = firstLineSplits[1:]
+        if not (len(firstLineSplits) > 0 and not firstLineSplits[0].isnumeric()):
+            return stateNames
+
+        for split in firstLineSplits:
+            stateNames = stateNames + [split.replace("\"", "").replace("\n", "")]
+
+
+    return stateNames
+
+def getInitialState(stateList, startLocation = DEFAULT_SPAWN):
+    startLocationPosition = stateList.index(startLocation)
+    return [[] for _ in range(startLocationPosition)] + [[0]] + [[] for _ in range(len(stateList)-startLocationPosition-1)]
+
+def findPoIs(locations):
+    pois = []
+    for loc in locations:
+
+        if not (loc.endswith("Top") or loc.endswith("Right") or loc.endswith("Bottom") or loc.endswith("Left") or loc.endswith("Middle")):
+            pois += [loc]
+
+    return pois
+
+
+def findFinalState(table, initialState):
+    oldState = initialState
+    currentState = iterate(table,oldState)
+
+    while getTableLine(oldState) != getTableLine(currentState):
+        oldState = currentState
+        currentState = iterate(table, oldState)
+
+    return currentState
+
+def findSubIndex(fullList, subList):
+    indices = []
+
+    for i in range(len(fullList)):
+        if fullList[i] in subList:
+            indices += [i]
+
+    return indices
+
+def reduceRequirementTable(table, labels, reducedLocations = None):
+    if reducedLocations == None:
+        reducedLocations = []
+        for label in labels:
+            if label.startswith("\"Pickup:") or label.startswith("\"Spawn:"):
+                reducedLocations += [label]
+
+    reducedTable = [[] for _ in range(len(reducedLocations))]
+    reducedIndex = findSubIndex(labels, reducedLocations)
+
+    print(f'(0/{len(reducedLocations)})')
+    for i in range(len(reducedLocations)):
+        startLocation = reducedLocations[i]
+        initialState = getInitialState(labels, startLocation)
+        finalState = findFinalState(table, initialState)
+        reducedTable[i] = [finalState[x] for x in reducedIndex]
+        print(f'({i+1}/{len(reducedLocations)})')
+
+    return reducedTable, reducedLocations
 
